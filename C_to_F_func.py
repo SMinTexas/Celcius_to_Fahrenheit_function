42ffdc6e3ba0c421dea619e0a3afaac907439247
Steven Murray <tstevenm@gmail.com>
2019-09-05 07:00:31 -0500

    Updated spelling

--- C_to_F_func.py
+++ C_to_F_func.py
@@ -1,4 +1,4 @@
-# Write a function that takes a temperature in Celcius, converts it to Fahrenheit,
+# Write a function that takes a temperature in Celsius, converts it to Fahrenheit,
 # and returns the value.
 #
 # The formula to convert a temperature from Celsius to Fahrenheit is:
@@ -8,5 +8,5 @@ def c_to_f(C):
     return (C * 9/5) + 32
 
 # Main program flow
-Celcius = float(input("Please enter today's temperature in Celcius "))
-print(f"Today's temperature in Fahrenheit will be {c_to_f(Celcius)} degrees.")
+Celsius = float(input("Please enter today's temperature in Celsius "))
+print(f"Today's temperature in Fahrenheit will be {c_to_f(Celsius)} degrees.")
